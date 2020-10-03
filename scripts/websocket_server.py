236154e014cfb537ea3285a9ef9a3bec687dbff7
root <root@localhost.localdomain>
2020-10-03 12:55:25 +0700

    new

--- scripts/websocket_server.py
+++ scripts/websocket_server.py
@@ -5,13 +5,17 @@ import asyncio
 # ======================================================
 
 class WSC_Server:
+
     def __init__(self, ip, port):
         self.clients = set()
+        self.list_clinets = []
         self.loop = asyncio.get_event_loop()
-        self._client_timeout = 5
+        self._client_timeout = 1
         self._wake_up_task = None
         self.ip = ip
-        self.port = port 
+        self.port = port
+        self.mess = []
+        self.full_client = 3
 
     # ======================================================
 
@@ -19,63 +23,103 @@ class WSC_Server:
         print("listening on {}:{}".format(self.ip, self.port))
         
         ws_server = websockets.serve(self.connect_client, self.ip, self.port)
+
         self.loop.run_until_complete(ws_server)
 
-        self._wake_up_task = asyncio.ensure_future(test())
+        # self._wake_up_task = asyncio.ensure_future(test())
 
         try:
             self.loop.run_forever()
         except KeyboardInterrupt:
             print ('caught keyboard interrupt')
-            self.exit()
+            # self.exit()
     
     # ======================================================
     
     async def connect_client(self, client: WebSocketServerProtocol, path):
         self.clients.add(client)
-        print('new client connected from {}:{}'.format(*client.remote_address))
-        
-        keep_alive_task = asyncio.ensure_future(self.keep_alive(client))
+        self.list_clinets.append(client.remote_address)
 
-        try:
-            await self.handle_messages(client)
-        except websockets.ConnectionClosed:
-            keep_alive_task.cancel()
+        print('new client connected from {}:{}'.format(*client.remote_address))
+        print(self.list_clinets)
+        print(len(self.list_clinets))
+
+        if len(self.list_clinets) > self.full_client:
+            print ('full client')
             await self.disconnect_client(client)
+
+        else:
+            keep_alive_task = asyncio.ensure_future(self.keep_alive(client))
+            asyncio.ensure_future(self.handle_authen(client))
+            try:
+                await self.handle_messages(client)
+            except websockets.ConnectionClosed:
+                keep_alive_task.cancel()
+                await self.disconnect_client(client)
 
     # ======================================================
     
     async def handle_messages(self, client):
         while True:
-            message = await client.recv()
-            print('recieved message from {}:{}: {}'.format(
-                *client.remote_address, message))
-            await asyncio.wait([client.send(message) for client in self.clients])
+            self.mess = await client.recv()
+
+            # print('recieved message from {}:{}: {}'.format(
+            #     *client.remote_address, self.mess))
+
+            # await asyncio.wait([client.send(self.mess) for client in self.clients]
+     
+    # ======================================================
+
+    
+            
+    # ======================================================
+    async def handle_authen(self, client):
+        # 
+
+        while True:
+            await asyncio.sleep(0.00000000001)
+            if len(self.mess) > 0:
+                print('mes : {}'.format(self.mess))
+                print('len : {}'.format(len(self.mess)))
+
+                await client.send(self.mess)
+
+                self.mess = ''
+
+            # await asyncio.sleep(self._client_timeout)
+
+            # print('mes : {}'.format(self.mess))
+            # 
+            # self.mess = ''
+ 
 
     # ======================================================
+
     async def disconnect_client(self, client):
-        await client.close()
         self.clients.remove(client)
-        print('client {}:{} disconnected'.format(
-            *client.remote_address))
+        self.list_clinets.remove(client.remote_address)
+        print('client {}:{} disconnected'.format(*client.remote_address))
+        await client.close()
     
     # ======================================================
     async def keep_alive(self, client: WebSocketServerProtocol):
+        n = 0 
         while True:
+            n = n + self._client_timeout
             await asyncio.sleep(self._client_timeout)
             try:
-                print('pinging {}:{}'.format(
-                    *client.remote_address))
+                print('pinging {}:{}'.format( *client.remote_address))
                 await asyncio.wait_for(client.ping(), self._client_timeout)
+                await client.send(str(n))
+
             except asyncio.TimeoutError:
-                print('client {}:{} timed out'.format(
-                    *client.remote_address))
+                print('client {}:{} timed out'.format( *client.remote_address))
                 await self.disconnect_client(client)
 
     # ======================================================
     def exit(self):
         print("exiting")
-        self._wake_up_task.cancel()
+        # self._wake_up_task.cancel()
         try:
             self.loop.run_until_complete(self._wake_up_task)
         except asyncio.CancelledError:
