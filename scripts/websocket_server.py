38e11a36ae8097364e8f09a22e37f893af599dd7
Phạm Trung Hiếu <phamhieu078@gmail.com>
2020-10-09 08:54:34 +0000

    update : authen donne

--- scripts/websocket_server.py
+++ scripts/websocket_server.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 import websockets
 from websockets import WebSocketServerProtocol
 import asyncio
@@ -5,6 +6,17 @@ import asyncio
 import mysql.connector
 import json
 import bcrypt
+
+
+if True:  # Include project path
+    import sys
+    import os
+    ROOT = os.path.dirname(os.path.abspath(__file__))+"/../"
+    CURR_PATH = os.path.dirname(os.path.abspath(__file__))+"/"
+    # sys.path.append(ROOT)
+
+    print(ROOT)
+    print(CURR_PATH)
 
 # ======================================================
 
@@ -19,10 +31,12 @@ class WSC_Server:
         self.ip = ip
         self.port = port
         self.mess =''
-        self.full_client = 3
+        self.full_client = 5
         self.list_clinets_provider = []
         self.authen: object
         self.status_check = True
+
+        self.dict_id_index_client = {}
         # ================================================
         self.check_True = '{"authen":"true"}'
         self.check_False = '{"authen":"false"}'
@@ -49,15 +63,25 @@ class WSC_Server:
         try:
             self.loop.run_forever()
         except KeyboardInterrupt:
-            print (' > keyboard interrupt')
+
+            for x in self.dict_id_index_client:
+                print(x)
+                var = self.dict_id_index_client[x]
+                self.Execute_sql_update(self.Update_status('users', 'status', 'id', str(var), 'offline'))
+            
+            print(' > keyboard interrupt')
+
+
             # self.exit()
     
     # ======================================================
     
     async def connect_client(self, client: WebSocketServerProtocol, path):
         # ======================================================
+
+        # print(client)
+
         # ======================================================
-        
         self.status_check = True        
         self.clients.add(client)
         self.list_clinets.append(client.remote_address)
@@ -78,26 +102,27 @@ class WSC_Server:
             authen =  asyncio.ensure_future(self.send_authen_check(client))
             # asyncio.ensure_future(self.handle_provider(client))
             # asyncio.ensure_future(self.controll_handle(client))      
-                
+
+            # ====================================================================== 
             try:
                 await asyncio.ensure_future(self.handle_messages_input(client, authen))
             except :
                 # keep_alive_task.cancel()
                 authen.cancel()
                 await self.disconnect_client(client)
-            finally:
-                print('> end ')
-        
     # ======================================================
 
-    async def handle_messages_input(self, client, authen):
+    async def handle_messages_input(self, client, authen ):
         async for mess in client:
             print('client {} messages :  {}'.format(client.remote_address, mess))
             # await self.authen_check(client, mess, authen)
-
+            # print(self.status_check)
+            
             if self.status_check == True:
                 await self.authen_check(client, mess, authen)
-            
+            else:
+                await self.handle_provider(client, mess)
+
 
         # while True:
         #     self.mess = await client.recv()
@@ -130,13 +155,13 @@ class WSC_Server:
             # sau 5 lần request
             await client.send(self.check_False)
             # disconnect_client
-            # await self.disconnect_client(client)
+            await self.disconnect_client(client)
 
         except :
             print ("> close send authen check ")
 
     # ======================================================
-    #  check messages == pass
+    #  check messages == pass  
     #  str = gửi pass :
     #  sau 3 lần gửi  : nếu không có gửi trời lại thì đóng kết nối
     # còn nếu có pass : check messages ,
@@ -151,9 +176,11 @@ class WSC_Server:
             messJson = json.loads(mess)
             id_userhw_json = messJson["id_userhw"]
             password_json = messJson["password"]
-            print('id = {}  \npassw = {}'.format(id_userhw_json, password_json))
+            # print('id = {}  \npassw = {}'.format(id_userhw_json, password_json))
 
         except:
+            await client.send(self.check_False)
+            await self.disconnect_client(client)
             print("> Not json")
 
 
@@ -170,81 +197,61 @@ class WSC_Server:
             status_of_userhw = data[0][1]
             password_of_userhw = data[0][6]
 
-            print (password_of_userhw)
+            print ('{} : {}'.format(id_index_of_userhw,status_of_userhw ))
 
         except:
             print('> Can not Execute_sql ')
-            
-
-        try:
-            if bcrypt.checkpw(bytes(password_of_userhw, 'utf-8'), bytes(password_json, 'utf-8')):
-                print("pass true")
-            else:
-                print("pass false")
-        except:
-            print ('> error')
-            
-        
-        # ============================================
-        # test database 
-        # ===========================================
-        # conver string to bytes
-        # b_password_json = bytes(password_json, 'utf-8')
-        # print(b_password_json)
-
-        # truy vấn id_userhw trong database
-        # try:
-        #     cursor = mydb.cursor()
-
-        #     cursor.reset()
-
-        #     # cursor.execute(self.Select_id('users', 'id_userhw', id_userhw_json))
-        #     sql = "SELECT * FROM users"
-        #     cursor.execute(sql)``
-
-        #     data = cursor.fetchall()
-        #     print(data)
-
-        #     cursor.close()
-        
-    
-        # except :
-        #     print('> search fail')
-        # finally:
-        #     password_of_userhw = 'error'
 
         # =======================================================
         #  nếu value_of_id_userhw không có data trả về => không có id_userhw trong database
+        try:
+            if len(data) != 0 and bcrypt.checkpw(bytes(password_json, 'utf-8'), bytes(password_of_userhw, 'utf-8')) and status_of_userhw == 'offline':
+            # if len(data) != 0 and bcrypt.checkpw(bytes(password_json, 'utf-8'), bytes(password_of_userhw, 'utf-8')) :
+                await client.send(self.check_Pass)
+                # stop status_check 
+                self.status_check = False
+                authen.cancel()
 
-        # try:
-        #     if len(value_of_id_userhw) != 0 and bcrypt.checkpw(b_password_json, b_password_of_userhw) and status_of_userhw == 'offline':
+                # ======================================================
 
-        #         await client.send(self.check_Pass)
-        #         # stop status_check 
-        #         self.status_check = False
-        #         authen.cancel()
+                #  example sql = "UPDATE `users` SET `status` = \'online\' WHERE `users`.`id` = 44";
 
+                #  Update_status(self, table_name, column_name, id_ , index , value)
 
-        #         # print(value_of_id_userhw)
+                self.Execute_sql_update(self.Update_status('users', 'status', 'id', str(id_index_of_userhw), 'online'))
 
-        #         # ======================================================
-        #         self.list_clinets_provider.append(client.remote_address)
-        #         # ======================================================
-        #     else:
-        #         print('fail')
-        #         await client.send('clone connect')
-        #         await self.disconnect_client(client)
+                print('{} = {}'.format(client.remote_address, id_index_of_userhw))
 
-        # except:
-        #     print("fail")
+                buffer_dict = {
+                    client.remote_address : id_index_of_userhw
+                }
+                self.dict_id_index_client.update(buffer_dict)
+
+            
+                # print(self.Update_status('users', 'status', 'id', '44', 'online'))
+                # ======================================================
+
+                # ======================================================
+                self.list_clinets_provider.append(client.remote_address)
+                # ======================================================
+            else:
+                print(self.check_False)
+                await client.send(self.check_False)
+                await self.disconnect_client(client)
+
+        except:
+            print("> ! fail")
         
     # ======================================================
     # client pulls data 
     # ghi data vao trong database
 
-    async def handle_provider(self, client ):
-        while True:
-            await asyncio.sleep(3)
+    async def handle_provider(self, client, mess):
+        await client.send('handle provider send : {}'.format(mess))
+        print('handle_provider_dicts = {} '.format(self.dict_id_index_client))
+        
+
+        
 
     # ======================================================
 
@@ -252,6 +259,13 @@ class WSC_Server:
         self.clients.remove(client)
         self.list_clinets.remove(client.remote_address)
         # self.list_clinets_provider.remove()
+
+        if (self.dict_id_index_client.get(client.remote_address) != None) :
+            var = self.dict_id_index_client[client.remote_address]
+            self.dict_id_index_client.pop(client.remote_address)
+            self.Execute_sql_update(self.Update_status('users', 'status', 'id', str(var), 'offline'))
+
+        # ======================================================
         print('client {} disconnected'.format(client.remote_address))
         await client.close()
     
@@ -288,7 +302,6 @@ class WSC_Server:
     async def controll_handle(self, client):
         print('controll handle')
      
-    
     # =================================================== 
     # =================== My SQL ========================
     # ===================================================
@@ -309,13 +322,22 @@ class WSC_Server:
         cursor = mydb.cursor()
 
         cursor.execute(sql)
-        
         data = cursor.fetchall()
 
         cursor.close()
         mydb.close()
 
         return data
+
+    def Execute_sql_update(self, sql):
+        mydb = self.config_mysql()
+        cursor = mydb.cursor()
+
+        cursor.execute(sql)
+        mydb.commit()
+
+        cursor.close()
+        mydb.close()
 
     # ===================================================
 
@@ -335,11 +357,10 @@ class WSC_Server:
 
     # ===================================================
 
-    # def Update_status(self, table_name, column_name, value,index):
-    #     # sql = "UPDATE `users` SET `status` = \'offline\' WHERE `users`.`id`
-    #     sql = "UPDATE `{}` SET `{}` = \'{}\' WHERE `{}`.`{}` = {}" \
-    #         .format(table_name)
-    #     return sql
+    def Update_status(self, table_name, column_name, id_ , index , value):
+        #  example sql = "UPDATE `users` SET `status` = \'online\' WHERE `users`.`id` = 44";
+        sql = "UPDATE `"+table_name+"` SET `"+column_name+"` = '"+value+"' WHERE `"+table_name+"`.`"+id_+"` = "+index+""
+        return sql
         
         
 
